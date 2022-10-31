ffedf8c26a5de7ec9efa662275c5c92b40146ec1
joshwhittick <xk18811@bristol.ac.uk>
2022-10-31 12:46:34 +0000

    make db updated for postgre for heroku

--- make_SQL_DB_with_pyth.py
+++ make_SQL_DB_with_pyth.py
@@ -11,3 +11,5 @@ conn = psycopg2.connect(
 mycursor = conn.cursor()
 
 mycursor.execute("CREATE TABLE Lifts (Exercise_ID serial NOT NULL, Date_of_exercise date NOT NULL, Exercise varchar(20) NOT NULL, Sets smallint NOT NULL, Reps smallint NOT NULL, Weight float NOT NULL, Total_Reps smallint NOT NULL, Total_Load float NOT NULL, PRIMARY KEY (Exercise_ID))")
+
+conn.commit()
