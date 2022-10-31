f18d57e33b66293a7c4e0a80ae7de61dab8e4d8c
joshwhittick <xk18811@bristol.ac.uk>
2022-10-31 16:53:20 +0000

    Updates

--- Lifting_DB_Main_Page.py
+++ Lifting_DB_Main_Page.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import sys
+
 sys.path.append(".")
 from DB_SQL_STREAMLIT_FUNCTIONS import total_lifted_for_exercise, total_lifted_for_exercise_in_date_range, all_instances_of_exercise_x, all_instances_of_exercise_x_in_date_range, plot_results, write_to_log
 
