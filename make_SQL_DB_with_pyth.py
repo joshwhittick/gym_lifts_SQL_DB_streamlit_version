20d6debd4d91b42103e8e6f194004e5c4ad99bb7
joshwhittick <xk18811@bristol.ac.uk>
2022-10-31 12:39:16 +0000

    make db updated for postgre for heroku

--- make_SQL_DB_with_pyth.py
+++ make_SQL_DB_with_pyth.py
@@ -2,12 +2,12 @@ import psycopg2
 from datetime import date
 
 conn = psycopg2.connect(
-    host='localhost', 
-    user='root',
-    password='YOUR_OWN_PASSWORD',
-    database='MY_LIFTS'
-    )
+    host='ec2-54-228-125-183.eu-west-1.compute.amazonaws.com', 
+    user='clokjbklnxevbe',
+    password='1211673e1d579c5db019e37f1ce28b2894f5d0f8b14415dc82c116e725562442',
+    database='d91nc5sad12f6v',
+    port='5432')
 
 mycursor = conn.cursor()
 
-mycursor.execute("CREATE TABLE `Lifts` (`Exercise_ID` int NOT NULL AUTO_INCREMENT,`Date_of_exercise` date NOT NULL, `Exercise` varchar(20) NOT NULL,`Sets` smallint NOT NULL, `Reps` smallint NOT NULL,`Weight` float NOT NULL,`Total_Reps` smallint NOT NULL,`Total_Load` float Not NULL,PRIMARY KEY (exercise_ID))")
+mycursor.execute("CREATE TABLE Lifts (Exercise_ID serial NOT NULL, Date_of_exercise date NOT NULL, Exercise varchar(20) NOT NULL, Sets smallint NOT NULL, Reps smallint NOT NULL, Weight float NOT NULL, Total_Reps smallint NOT NULL, Total_Load float NOT NULL, PRIMARY KEY (Exercise_ID))")
