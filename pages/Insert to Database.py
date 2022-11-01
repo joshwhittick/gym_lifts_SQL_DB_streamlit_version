32ea2ddd5929776cfd065220585a8275a2de8166
Josh Whittick <92024297+joshwhittick@users.noreply.github.com>
2022-11-01 13:42:34 +0000

    Update Insert to Database.py

--- pages/Insert to Database.py
+++ pages/Insert to Database.py
@@ -6,7 +6,7 @@ from DB_SQL_STREAMLIT_FUNCTIONS import total_lifted_for_exercise, total_lifted_f
 st.markdown("# Insert to Database")
 
 date = st.text_input("Enter a date of gym session (YYYY-MM-DD):")
-st.caption("Enter values for all exercises perfomed that day top enter and then commit to DB")
+st.caption("Enter values for an exercises perfomed on that day, then commit to DB and repeat for all exercises")
 
 with st.form("Insert values for lift", clear_on_submit=True):
   lift = st.text_input("enter lift")
@@ -18,4 +18,3 @@ with st.form("Insert values for lift", clear_on_submit=True):
   if st.form_submit_button() == True:
     st.text("Data commited to DB")
     write_to_log(date, lift, sets, reps, load, total_reps, total_load)
-    
