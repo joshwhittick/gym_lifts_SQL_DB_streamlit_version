182895126ef78176696fcf357a18bcb269d8d803
Josh Whittick <92024297+joshwhittick@users.noreply.github.com>
2022-11-01 13:45:46 +0000

    Update Lifting_DB_Main_Page.py

--- Lifting_DB_Main_Page.py
+++ Lifting_DB_Main_Page.py
@@ -5,4 +5,4 @@ sys.path.append(".")
 from DB_SQL_STREAMLIT_FUNCTIONS import total_lifted_for_exercise, total_lifted_for_exercise_in_date_range, all_instances_of_exercise_x, all_instances_of_exercise_x_in_date_range, plot_results, write_to_log
 
 st.title('Main Page')
-st.subheader("Navigate to the other pages to: insert lifts and data to a database or to interogate the database")
+st.subheader("Navigate to the other pages to either insert lifts and data to a database or to interogate the database")
